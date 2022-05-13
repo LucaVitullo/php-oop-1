7729404b0d30a37fcc7017c8ebd382dcfc9df866
Lucavit9 <95365833+Lucavit9@users.noreply.github.com>
2022-05-13 15:58:18 +0200

    aggiunta immagine

--- index.php
+++ index.php
@@ -10,6 +10,7 @@
 </head>
 
 <body>
+
     <?php
     /*Oggi pomeriggio ripassate i primi concetti di classe e di variabili e
      metodi d'istanza che abbiamo visto stamattina e create un file index.php in cui:
@@ -23,6 +24,7 @@
     {
         public $nome;
         public $genere;
+        public $path;
         public $protagonista;
 
         function __construct($_genere)
@@ -37,6 +39,9 @@
                 "<div class='container'>
                     <h3>Nome film:</h3>
                     <span>" . $this->nome . "</span>
+                    <div class='image_path'>
+                        <img src=" . $this->path . ">
+                    </div>
                     <h3>Nome protagonista:</h3>
                     <span>" . $this->protagonista . "</span>
                     <h3>Genere:</h3>
@@ -46,12 +51,15 @@
     }
     $halloween = new Movie('Horror');
     $halloween->nome = 'Halloween';
+    $halloween->path = 'https://midnightfactory.it/wp-content/uploads/2019/11/beef9331-halloween-front.jpg';
     $halloween->protagonista = 'Michael Myers';
 
 
 
     $spiderman = new Movie('Fantascienza');
     $spiderman->nome = 'Spiderman';
+    $spiderman->path = 'https://www.tomshw.it/images/images/2021/11/spider-man-no-way-home-198068.jpg';
+
     $spiderman->protagonista = 'Peter Parker';
 
 
